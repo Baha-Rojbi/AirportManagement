939e838e4d97c05d7af3b11b9d078e6caff75f5c
Baha Rojbi <101024667+Baha-Rojbi@users.noreply.github.com>
2023-11-14 14:31:52 +0100

    added I iServices

---
+++ AM.ApplicationCore/Interfaces/IServicePlane.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AM.ApplicationCore.Domain;
+
+namespace AM.ApplicationCore.Interfaces
+{
+    public interface IServicePlane :IService<Plane>
+    {
+    }
+}
