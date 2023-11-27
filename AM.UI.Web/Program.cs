1db1e98b2738509edaa78479e8ddc10cda1a05f6
Baha Rojbi <101024667+Baha-Rojbi@users.noreply.github.com>
2023-11-27 21:54:24 +0100

    Recuperer l id d un apartment et l afficher du lien

--- AM.UI.Web/Program.cs
+++ AM.UI.Web/Program.cs
@@ -1,7 +1,16 @@
+using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Services;
+using AM.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DbContext, AMContext>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IServiceFlight, ServiceFlight>();
+builder.Services.AddSingleton<Type>(t => typeof(GenericRepository<>));
 
 var app = builder.Build();
 
